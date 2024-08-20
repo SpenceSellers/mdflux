15bdd48fde63fcab4f11daff19c22d615c7ab7d1
Spencer Sellers <spencesellers@gmail.com>
2024-08-20 10:20:21 -0500

    Add more tests around update arguments

--- tests/test_update.py
+++ tests/test_update.py
@@ -29,3 +29,90 @@ Test String
 [mdflux end]: #
 """
 
+def test_tags_ignored_in_code_blocks():
+    markdown = """
+# Heading
+```
+[mdflux]: # (echo "Test String")
+[mdflux end]: #
+```
+# Heading after
+"""
+
+    res = mdflux.apply_updatemd_str(markdown, "test-file.md")
+
+    assert markdown == res
+
+def test_code_argument():
+    markdown = """
+[mdflux code]: # (echo "Test String")
+[mdflux end]: #
+"""
+
+    expected_res = """
+[mdflux code]: # (echo "Test String")
+```
+Test String
+```
+[mdflux end]: #
+"""
+
+    res = mdflux.apply_updatemd_str(markdown, "test-file.md")
+
+    assert res == expected_res
+
+def test_code_argument_with_language():
+    markdown = """
+[mdflux code=javascript]: # (echo "Test String")
+[mdflux end]: #
+"""
+
+    expected_res = """
+[mdflux code=javascript]: # (echo "Test String")
+```javascript
+Test String
+```
+[mdflux end]: #
+"""
+
+    res = mdflux.apply_updatemd_str(markdown, "test-file.md")
+
+    assert res == expected_res
+
+def test_markdown_is_escaped_by_default():
+    markdown = """
+[mdflux]: # (echo "_italics_")
+[mdflux end]: #
+"""
+
+    expected_res = """
+[mdflux]: # (echo "_italics_")
+\\_italics\\_
+
+[mdflux end]: #
+"""
+
+    res = mdflux.apply_updatemd_str(markdown, "test-file.md")
+
+    assert res == expected_res
+
+def test_markdown_in_markdown():
+    markdown = """
+[mdflux markdown]: # (echo "_italics_")
+[mdflux end]: #
+"""
+
+    expected_res = """
+[mdflux markdown]: # (echo "_italics_")
+_italics_
+
+[mdflux end]: #
+"""
+
+    res = mdflux.apply_updatemd_str(markdown, "test-file.md")
+
+    assert res == expected_res
+
+
+
+
